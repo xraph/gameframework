4bfa9e201ee276fbb75cd6fc13eb3a319cd445da
Rex Raphael <rex.raphael@outlook.com>
2025-10-26 18:03:12 -0500

    feat: Initial project scaffolding
    Creates the initial file structure and configuration for the Flutter plugin project.
    
    This includes:
    - Root and Android-specific `.gitignore` files.
    - Basic `pubspec.yaml` defining the project as a multi-platform plugin.
    - Default `analysis_options.yaml` enabling `flutter_lints`.
    - Placeholder `README.md`, `CHANGELOG.md`, and `LICENSE` files.

---
+++ example/android/app/src/main/kotlin/com/xraph/gameframework/gameframework_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.xraph.gameframework.gameframework_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import gameframework
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  GameframeworkPlugin.register(with: registry.registrar(forPlugin: "GameframeworkPlugin"))
+}
